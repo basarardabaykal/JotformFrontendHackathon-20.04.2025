95e282d834bbc24b4496f72a2cb6b967e52a55a8
Başar Arda Baykal <baykalbasararda@gmail.com>
2025-04-20 12:59:35 +0300

    controlling the quantity

--- src/components/Card.jsx
+++ src/components/Card.jsx
@@ -1,16 +1,33 @@
-import temp from "../../public/temp.jpg"
+import { useState } from "react"
 
 export default function Card({title, price, description, image}) {
+    const [quantity, setQuantity] = useState(0);
+
+    const decreaseQuantity = () => {
+        if (quantity > 0) {
+            setQuantity(quantity-1);
+        }
+    }
+
+    const increaseQuantity = () => {
+        setQuantity(quantity+1);
+    }
+
     return (
-        <div className="bg-white text-black mb-4 rounded-2xl p-2 h-[400px] shadow-2xl shadow-gray-600">
-            <div>
+        <div className="bg-white text-black mb-4 rounded-2xl p-2 h-[420px] shadow-2xl shadow-gray-600">
+            <div className="flex flex-col justify-center">
                 <section id="image" className="w-[320px] h-[240px] overflow-hidden flex justify-center items-center">
                     <img className="w-[150px]" src={image} alt="" />
                 </section>
-                <section id="text" className="h-[100px]">
+                <section id="text" className="h-[100px] flex flex-col items-center justify-center text-lg">
                     <p>{title}</p>
                     <p>{description}</p>
                     <p>{price}</p>
+                </section>
+                <section className="text-2xl flex flex-row items-center mx-auto">
+                    <button onClick={() => decreaseQuantity()}>-</button>
+                    <p className="mx-2">{quantity}</p>
+                    <button onClick={() => increaseQuantity()}>+</button>
                 </section>
             </div>
             
